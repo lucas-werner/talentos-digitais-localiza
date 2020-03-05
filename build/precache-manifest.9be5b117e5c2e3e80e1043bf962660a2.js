66ba3999206ebdfc07c04c739ad20b05ae72abae
Lucas-Werner <lucas.1werner@gmail.com>
2020-03-05 16:11:00 -0300

    new cars

--- build/precache-manifest.9be5b117e5c2e3e80e1043bf962660a2.js
+++ build/precache-manifest.9be5b117e5c2e3e80e1043bf962660a2.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "61ad2c521dfbeb4f9cf4e1867fb070a4",
+    "revision": "5e9baf222df27f3ea397e6013d953837",
     "url": "/projeto-localiza/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/projeto-localiza/static/css/2.64a99b10.chunk.css"
   },
   {
-    "revision": "a2e9f219b26d488262c9",
+    "revision": "4984bde1ddf3214147fb",
     "url": "/projeto-localiza/static/css/main.439b9e70.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/projeto-localiza/static/js/2.71303cb2.chunk.js"
   },
   {
-    "revision": "a2e9f219b26d488262c9",
-    "url": "/projeto-localiza/static/js/main.cd61fbc0.chunk.js"
+    "revision": "4984bde1ddf3214147fb",
+    "url": "/projeto-localiza/static/js/main.2b0883a5.chunk.js"
   },
   {
     "revision": "45161d70e8da23f00556",
