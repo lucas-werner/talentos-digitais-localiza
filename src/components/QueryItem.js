d3d7e99e85855bb71f71c2780d79286b610646a2
Lucas-Werner <lucas.1werner@gmail.com>
2020-03-05 11:13:39 -0300

    acertando querylist

--- src/components/QueryItem.js
+++ src/components/QueryItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import './queryList.css';
 
 
 class ProductItem extends Component {
@@ -8,13 +9,14 @@ class ProductItem extends Component {
     const {
       title, thumbnail, price, id, quantity, realPrice, productInfo,
     } = this.props;
+    console.log(productInfo)
     return (
       <div className="row">
         <div className="col s12 m7">
-          <div className="card car-card">
+          <div className="card car-card-query">
             <div className="card-image">
               <img alt="Car Cover" className="car-card-image" src={thumbnail} />
-              <span className="card-title">{title}</span>
+              <span className="card-title-query">{title}</span>
             </div>
             <div className="card-content">
               <p>{price}</p>
